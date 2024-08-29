47e89d45c565d358388686eae6769d6d9edca308
Diego Garcia <gdiego416@gmail.com>
2024-08-29 19:43:53 -0300

    feat: routes GET /cliente and GET /cliete/{id}

--- src/main/java/br/edu/umfg/prog/diegogarcia/api/controllers/ClienteController.java
+++ src/main/java/br/edu/umfg/prog/diegogarcia/api/controllers/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/cliente")
@@ -18,5 +19,23 @@ public class ClienteController {
     ) {
         clientes.add(cliente);
         return ResponseEntity.ok(cliente);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> get() {
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> get(
+            @PathVariable UUID id
+    ) {
+        for (var cliente : clientes) {
+            if (cliente.getId().equals(id)) {
+                return ResponseEntity.ok(cliente);
+            }
+        }
+
+        return null;
     }
 }
